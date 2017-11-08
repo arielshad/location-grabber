a4837125fb419753a36225efe47cb36e6a1fd1e7
Ariel Shadkhan <shad.ariel@gmail.com>
2017-11-08 17:38:28 +0200

    get ip and get location

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const app = express()
+const iplocation = require('iplocation')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const ipMiddleware = function(req, res, next) {
+    const clientIp = requestIp.getClientIp(req); 
+    next();
+};
+
+
+
+app.get('/', (req, res) => {
+	return iplocation(req.clientIp, function (error, res) {
+		return res.country;
+	}
+	
+	
+})
 
 
 
