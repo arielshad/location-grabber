0416a9fdccc4ca36691436ab03a3dcb767b506a0
Ariel Shadkhan <shad.ariel@gmail.com>
2017-11-08 17:49:30 +0200

    add loction as answer

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const iplocation = require('iplocation')
 
+app.enable('trust proxy')
+
+
 const ipMiddleware = function(req, res, next) {
     const clientIp = requestIp.getClientIp(req); 
     next();
@@ -9,16 +12,18 @@ const ipMiddleware = function(req, res, next) {
 
 
 
+
+
 app.get('/', (req, res) => {
-	return iplocation(req.clientIp, function (error, res) {
-		return res.country;
-	});
 	
+	iplocation(req.ip).then(result=>{
+		return res.send(result);
+	})
 	
 })
 
 
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3001, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
