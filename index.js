ebe1e4b9ea74f2460f81729c85536b51c207b75e
Ariel Shadkhan <shad.ariel@gmail.com>
2017-11-08 17:58:49 +0200

    write location to file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const iplocation = require('iplocation')
+const fs = require('fs');
+
 
 app.enable('trust proxy')
 
@@ -17,6 +19,7 @@ const ipMiddleware = function(req, res, next) {
 app.get('/', (req, res) => {
 	
 	iplocation(req.ip).then(result=>{
+		fs.writeFileSync(`./locations/${+new Date}.json`, JSON.stringify(result,null, 3));
 		return res.send(result);
 	})
 	
