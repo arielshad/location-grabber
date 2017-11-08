022489014b2381e5097be23966dad497008f4a5d
Ariel Shadkhan <shad.ariel@gmail.com>
2017-11-08 17:31:56 +0200

    change to env port

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 80!'))
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
